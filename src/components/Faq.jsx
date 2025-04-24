8620da3ed08d939c7e740c597f86d2257e7c68e2
Ifeoluwa <deleoyeroifeoluwa@gmail.com>
2025-04-24 12:04:35 +0100

    Updated Faq

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,6 +1,8 @@
 
 import React, { useState } from 'react';
 import '../styles/Faq.css';
+import CloseIcon from '../assets/closed-faq.svg';
+import OpenIcon from '../assets/open-faq.svg';
 
 const FAQ = () => {
   const [openIndex, setOpenIndex] = useState(0);
@@ -67,16 +69,9 @@ const FAQ = () => {
               {item.question}
               <div className="faq-icon">
                 {openIndex === index ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="8" y1="12" x2="16" y2="12"></line>
-                  </svg>
+                  <img src={CloseIcon} alt="Close" className="faq-icon-close" />
                 ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="12" y1="8" x2="12" y2="16"></line>
-                    <line x1="8" y1="12" x2="16" y2="12"></line>
-                  </svg>
+                  <img src={OpenIcon} alt="Close" className="faq-icon-close" />
                 )}
               </div>
             </div>
